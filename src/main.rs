81c0a207badd2f88f17f9997930d3373c21e9f59
Aldorithms <99375833+aldorithms@users.noreply.github.com>
2023-05-08 15:54:53 -0700

    get

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use sfml::graphics::{Color, RenderTarget, RenderWindow, Sprite, Texture};
+use sfml::graphics::{Color, RenderTarget, RenderWindow, Sprite, Texture, Texture::from_file};
 use sfml::system::Vector2f;
-use sfml::window::{Event, Key, Style};
+use sfml::window::{Event, Key, Style::CLOSE};
+use sfml::SfBox;
 
 
 
 fn main() {
-    let mut window: RenderWindow = RenderWindow::new((800, 600),"SFML Example", Style::CLOSE,&Default::default());
+    let mut window: RenderWindow = RenderWindow::new((800, 600),"SFML Example", CLOSE,&Default::default());
     window.set_vertical_sync_enabled(true);
-    let mut texture: sfml::SfBox<Texture> = Texture::from_file("assets/sfml-icon-small.png").unwrap();
+    let mut texture: SfBox<Texture> = from_file("assets/sfml-icon-small.png").unwrap();
     let mut sprite: Sprite = Sprite::with_texture(&texture);
     sprite.set_position(Vector2f::new(100., 100.));
     sprite.set_scale(Vector2f::new(0.5, 0.5));
